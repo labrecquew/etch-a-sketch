c6c1e15a175a4658e123d23e0ab9e2d559405c65
labrecquew <labrecque.weston@gmail.com>
2025-02-27 15:57:11 -0500

    Set up the "hover" effect on each grid div

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -7,8 +7,11 @@ function createGridLayout(gridNumber) {
     let gridSize = 500 / gridNumber;
     for (let i = 0; i < totalGrids; i++) {
         let grid = document.createElement('div');
-        grid.style.height = gridSize + "px"
-        grid.style.width = gridSize + "px"
+        grid.style.height = gridSize + "px";
+        grid.style.width = gridSize + "px";
+        grid.addEventListener("mouseover", function() {
+            grid.style.backgroundColor = "#ADD8E6";
+        });
         container.appendChild(grid);
     }
 };
